b1fe1b847d379c4c43096320379f65ee43de7bd6
JHurricane96 <arun.jhurricane@gmail.com>
2015-05-29 21:48:40 +0530

    [SUBMIT] Calc needs some science :(

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function events() {
 
 	var displayInfoButton = document.getElementById('button-info');	
 	var displayCalcButton = document.getElementById('button-calc');
-	var squeezeTime = 600;
+	var fadeTime = 500;
 
 	var infoContainer = document.getElementById('container-info');
 	var calcContainer = document.getElementById('container-calc');
@@ -11,11 +11,15 @@ function events() {
 	var pageTurnersContainer = document.getElementById('container-pagebuttons')
 	var pageLeft = document.querySelector('button#pageleft');
 	var pageRight = document.querySelector('button#pageright');
+	var pageIndicatorsContainer = document.getElementById('pageindicators')
 	var pageIndicators = document.querySelectorAll('#pageindicators *');
+	var pages = document.getElementById('pages');
 	var page1 = document.getElementById('page1');
 	var pageNo = 1;
 	var pageLeftLimit = 1;
 	var pageRightLimit = 4;
+	var infoPageResized = false;
+	var resizeInfoPageTimer;
 
 	var buttonPlace = document.getElementById('container-buttons');
 	var calcDeleteButton = document.getElementById('calc-button-delete');
@@ -35,13 +39,12 @@ function events() {
 	}
 
 	function _resizeCalcButtons() {
-		var buttonArea = document.getElementById('container-buttons');
 		var calcButtons = document.querySelectorAll('button.calc');
 
 		calcButtons = Array.prototype.slice.call(calcButtons, 0);
 
 		calcButtons.forEach(function(elt){
-			if (buttonArea.offsetWidth>buttonArea.offsetHeight) {
+			if (buttonPlace.offsetWidth>buttonPlace.offsetHeight) {
 				elt.style.height = "80%";
 				elt.style.width = elt.offsetHeight + 'px';
 			}
@@ -55,9 +58,9 @@ function events() {
 
 	function resizeCalcButtons() {
 		if (!resizeCalcButtonsTimer) {
-			resizeCalcButtons = true;
+			calcButtonsResized = true;
 			setTimeout(function() {
-				resizeCalcButtons = false;
+				calcButtonsResized = false;
 				_resizeCalcButtons();
 			}, 17);
 		}
@@ -79,7 +82,6 @@ function events() {
 			expr = expr.replace(/sin/g, 'Math.sin');
 			expr = expr.replace(/cos/g, 'Math.cos');
 			expr = expr.replace(/tan/g, 'Math.tan');
-			console.log(expr);
 			answer = eval(expr);
 			if(answer||answer===0) {
 				calcInput.value = answer;
@@ -129,15 +131,18 @@ function events() {
 	}
 	
 	function displayCalc() {
-		//calcContainer.style.opacity = 0;
+		calcContainer.style.opacity = 0;
 		pageTurnersContainer.style.position = "static";
 
 		displayCalcButton.removeEventListener("click", displayCalc);
+		window.removeEventListener('keydown', pageTurnWithKeys);
 		pageRight.removeEventListener("click", pageTurnRight);
 		pageLeft.removeEventListener("click", pageTurnLeft);
+		pageIndicatorsContainer.removeEventListener("click", pageTurn);
+		window.removeEventListener("resize", onInfoResize);
 
-		animate(function (t) {return t;}, squeezeTime/2, function (x) {
-			infoContainer.style.width = 100*(1 - x) + '%';
+		animate(function (t) {return t;}, fadeTime, function (x) {
+			infoContainer.style.opacity = 1 - x;
 		}, cleanup1);
 
 		function cleanup1() {
@@ -154,8 +159,8 @@ function events() {
 
 			_resizeCalcButtons();
 
-			animate(bounce, squeezeTime*1.5, function (x) {
-				calcContainer.style.width = x*100 + '%';
+			animate(function (t) {return t;}, fadeTime, function (x) {
+				calcContainer.style.opacity = x;
 				_resizeCalcButtons();
 			}, cleanup2);
 		}
@@ -230,10 +235,10 @@ function events() {
 		pageTurners.forEach(function (elt) {
 			elt.disabled = true;
 		});
+		window.removeEventListener('keydown', pageTurnWithKeys);
 
 		pageIndicators[pageNo-1].className = '';
 
-		var pages = document.getElementById('pages');
 		var x0 = parseInt(pages.style.marginLeft);
 		
 
@@ -250,6 +255,8 @@ function events() {
 			pageTurners.forEach(function (elt) {
 			elt.disabled = false;
 			});
+			_onInfoResize();
+			pageIndicatorsContainer.addEventListener("click", pageTurn);
 		}
 
 		animate(bounce, 1500, xToMarginLeft, cleanup);
@@ -267,10 +274,10 @@ function events() {
 		pageTurners.forEach(function (elt) {
 			elt.disabled = true;
 		});
+		window.removeEventListener('keydown', pageTurnWithKeys);
 
 		pageIndicators[pageNo-1].className = '';
 
-		var pages = document.getElementById('pages');
 		var x0 = parseInt(pages.style.marginLeft);
 		
 
@@ -287,6 +294,8 @@ function events() {
 			pageTurners.forEach(function (elt) {
 			elt.disabled = false;
 			});
+			_onInfoResize();
+			pageIndicatorsContainer.addEventListener("click", pageTurn);
 		}
 
 		animate(bounce, 1500, xToMarginLeft, cleanup);
@@ -295,30 +304,137 @@ function events() {
 		pageIndicators[pageNo-1].className = 'currentpage';
 	}
 
-	function onInfoResize() {
-		page1.style.marginTop = Math.floor((window.innerHeight - pageRight.offsetHeight - 60)/2 - page1.offsetHeight) + 'px';
+	function pageTurnWithKeys(event) {
+		if (event.keyCode==37)
+			pageTurnLeft();
+		else if (event.keyCode==39)
+			pageTurnRight();
+	}
+
+	function pageTurn(event) {
+		var newPageNo = event.target.id.slice(-1);
+		if(!/^pageindicator\d$/.test(event.target.id))
+			return;
+		pageIndicatorsContainer.removeEventListener("click", pageTurn);
+		if (newPageNo == pageNo) {
+			pageIndicatorsContainer.addEventListener("click", pageTurn);
+			return;
+		}
+		if (newPageNo-pageNo == 1) {
+			pageTurnRight();
+			return;
+		}
+		else if(newPageNo-pageNo == -1) {
+			pageTurnLeft();
+			return;
+		}
+
+		var newPage = document.getElementById('page' + newPageNo);
+		var pageTurners = document.getElementsByClassName('pageturn');
+		pageTurners = Array.prototype.slice.call(pageTurners, 0);
+		pageTurners.forEach(function (elt) {
+			elt.disabled = true;
+		});
+		window.removeEventListener('keydown', pageTurnWithKeys);
+		pageIndicators[pageNo-1].className = '';
+
+		var x0 = parseInt(pages.style.marginLeft);
+
+		if (newPageNo > pageNo) {
+			var afterCurrentPage = document.getElementById('page' + String(+pageNo+1));
+			pages.insertBefore(newPage, afterCurrentPage);
+
+			function xToMarginLeftTurnRight(x) {
+				if (isNaN(x0)) {
+						pages.style.marginLeft = -x*100 + '%';
+
+					}
+					else
+						pages.style.marginLeft = x0 - x*100 + '%';
+			}
+
+			function cleanup() {
+				pageTurners.forEach(function (elt) {
+				elt.disabled = false;
+				});
+				if (newPageNo==pageRightLimit)
+					pages.appendChild(newPage);
+				else
+					pages.insertBefore(newPage, document.getElementById('page' + String(+newPageNo+1)));
+				pages.style.marginLeft = String(100*(1-newPageNo)) + '%';
+				pageIndicatorsContainer.addEventListener("click", pageTurn);
+				_onInfoResize();
+			}
+
+			animate(bounce, 1500, xToMarginLeftTurnRight, cleanup);
+
+		}
+		else {
+			var currentPage = document.getElementById('page' + String(pageNo));
+			pages.insertBefore(newPage, currentPage);
+
+			function xToMarginLeftTurnLeft(x) {
+				if (isNaN(x0)) {
+						pages.style.marginLeft = x*100 + '%';
+
+					}
+					else
+						pages.style.marginLeft = x0 + x*100 + '%';
+			}
+
+			function cleanup() {
+				pageTurners.forEach(function (elt) {
+				elt.disabled = false;
+				});
+				pages.insertBefore(newPage, document.getElementById('page' + String(+newPageNo+1)));
+				pages.style.marginLeft = String(100*(1-newPageNo)) + '%';
+				pageIndicatorsContainer.addEventListener("click", pageTurn);
+				_onInfoResize();
+			}
+
+			animate(bounce, 1500, xToMarginLeftTurnLeft, cleanup);
+
+		}
+
+		pageNo = newPageNo;
+		pageIndicators[pageNo-1].className = 'currentpage';
+	}
+
+	function _onInfoResize() {
+		page1.style.marginTop = Math.floor((window.innerHeight - pageRight.offsetHeight - 60)/2 - page1.offsetHeight/1.5) + 'px';
 		if (parseInt(page1.style.marginTop)<0)
 			page1.style.marginTop=0;
 
 		if(window.innerHeight>window.innerWidth) {
+			window.removeEventListener('keydown', pageTurnWithKeys);
 			pages.style.marginLeft = '0';
 			pageIndicators[pageNo-1].className = '';
 			pageNo = 1;
-			console.log(pageNo);
 			pageIndicators[0].className = 'currentpage';
 		}
+		else
+			window.addEventListener('keydown', pageTurnWithKeys);
+	}
 
+	function onInfoResize() {
+		if (!resizeInfoPageTimer) {
+			infoPageResized = true;
+			setTimeout(function() {
+				infoPageResized = false;
+				_onInfoResize();
+			}, 17);
+		}
 	}
 
 	function displayInfo() {
-		//infoContainer.style.opacity = 0;
+		infoContainer.style.opacity = 0;
 
 		displayInfoButton.removeEventListener("click", displayInfo);
 		calcInput.removeEventListener("keypress", calcKeyboardInput);
 		buttonPlace.removeEventListener("click", calcButtonPress);
 		calcDeleteButton.removeEventListener("click", calcDeleteButtonPress);
 
-		animate(function (t) {return t;}, squeezeTime/3, function (x) {calcContainer.style.width = 100*(1 - x) + '%';}, cleanup1);
+		animate(function (t) {return t;}, fadeTime, function (x) {calcContainer.style.opacity = 1 - x;}, cleanup1);
 
 		function cleanup1() {
 			document.body.style.height = "";
@@ -326,25 +442,28 @@ function events() {
 			html.style.overflowY = "";
 			calcContainer.style.display = "none";
 			infoContainer.style.display = "block";
-			onInfoResize();
-			animate(bounce, squeezeTime*1.5, function (x) {infoContainer.style.width = 100*x + '%';}, cleanup2);
+			_onInfoResize();
+			animate(function (t) {return t;}, fadeTime, function (x) {infoContainer.style.opacity = x; _onInfoResize();}, cleanup2);
 		}
 
 		function cleanup2() {
 			window.removeEventListener("resize", resizeCalcButtons);
 			displayCalcButton.addEventListener("click", displayCalc);
+			window.addEventListener('keydown', pageTurnWithKeys);
 			pageRight.addEventListener("click", pageTurnRight);
 			pageLeft.addEventListener("click", pageTurnLeft);
 			window.addEventListener("resize", onInfoResize);
-			pageTurnersContainer.style.position = "fixed";
+			pageIndicatorsContainer.addEventListener("click", pageTurn);
 		}
 	}
 
-	onInfoResize();
-	window.addEventListener("resize", onInfoResize);
+	_onInfoResize();
+	window.addEventListener("resize", _onInfoResize);
 	displayCalcButton.addEventListener("click", displayCalc);
+	window.addEventListener('keydown', pageTurnWithKeys);
 	pageRight.addEventListener("click", pageTurnRight);
 	pageLeft.addEventListener("click", pageTurnLeft);
+	pageIndicatorsContainer.addEventListener("click", pageTurn);
 	infoContainer.style.opacity = 1.0;
 }
 
